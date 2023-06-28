3efeb5915a411768d0e64d86570419caa376a25c
Rachid F <rf@artesi.xyz>
2023-06-28 21:49:15 +0200

    added op proposer script

--- bash_scripts/run_opproposer.sh
+++ bash_scripts/run_opproposer.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
+# Find the Addresss of the L2 OutPutOracle
+L2OO_ADDR=$(cat /opstack/optimism/packages/contracts-bedrock/deployments/getting-started/L2OutputOracleProxy.json | jq -r '.address')
+echo $L2OO_ADDR
 
 code=$(curl -s -o /dev/null -w "%{http_code}" 'http://opnode:8547')
-echo "Code: "
+echo "OPProposer Code: "
 echo $code
 
 if [ "$code" = 200 ]; then
+    echo "Proposer - Opnode:8547 200. Launching Proposer "
     cd /opstack/optimism/op-proposer
-
     ./bin/op-proposer \
         --poll-interval 12s \
         --rpc.port 8560 \
