38b2788e4acf4adb0cc01db76d6c008120602ad9
Rachid F <rf@artesi.xyz>
2023-06-28 18:54:15 +0200

    adding opbatcher script. Replaced Localhost with Docker Service name

---
+++ bash_scripts/run_opbatcher.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#heathcheck opgeth, and run
+
+code=$(curl -s -o /dev/null -w "%{http_code}" 'http://opnode:8547')
+echo "Code: "
+echo $code
+
+if [ "$code" = 200 ]; then
+    echo "OpNode detected. Starting Batcher."
+    cd ~/optimism/op-batcher
+    ./bin/op-batcher \
+    --l2-eth-rpc=http://opgeth:8545 \
+    --rollup-rpc=http://opnode:8547 \
+    --poll-interval=1s \
+    --sub-safety-margin=6 \
+    --num-confirmations=1 \
+    --safe-abort-nonce-too-low-count=3 \
+    --resubmission-timeout=30s \
+    --rpc.addr=0.0.0.0 \
+    --rpc.port=8548 \
+    --rpc.enable-admin \
+    --max-channel-duration=1 \
+    --l1-eth-rpc=$L1_RPC \
+    --private-key=$BATCHER_KEY
+else
+    echo "Cannot detect Node... Batcher Waiting..."
+fi
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 let ejs = require('ejs');
 const fs = require('fs');
 const { exec } = require('child_process');
+const { Web3 } = require('web3');
 
 /*
 Builds the file "Getting Started.json" that contains necessary data to start an opstack rollup
@@ -36,6 +37,29 @@ function buildHardhatConfig() {
   };
 }
 
+
+async function getLatestFinalizedBlockNumber() {
+  try {
+    // Connect to an Ethereum node
+    const web3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/');
+
+    // Get the latest finalized block number
+    const block = await web3.eth.getBlockNumber('latest', true);
+
+    
+    console.log(parseInt(block, 16));
+
+    if (block && block.finality) {
+      const blockNumber = block.number;
+      console.log('Latest Finalized Block Number:', parseInt(blockNumber, 16));
+    } else {
+      console.log('Failed to retrieve the latest finalized block.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
 function prepareOPStack() {
   buildGettingStarted();
   buildHardhatConfig();
@@ -43,3 +67,6 @@ function prepareOPStack() {
 }
 
 prepareOPStack();
+
+// Call the function to retrieve the latest finalized block number
+//getLatestFinalizedBlockNumber();
