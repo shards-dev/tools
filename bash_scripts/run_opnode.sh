ae4b8b1d1c34b064ab028073d58adb605502b7a8
Rachid F <rf@artesi.xyz>
2023-06-28 17:18:19 +0200

    a

--- bash_scripts/run_opnode.sh
+++ bash_scripts/run_opnode.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #heathcheck opgeth, and run
 
-code=$(curl -s -o /dev/null -w "%{http_code}" 'http://localhost:8551')
-
+code=$(curl -s -o /dev/null -w "%{http_code}" 'http://opgeth:8551')
+echo "Code: "
 echo $code
 
 if [[ $code == 401 ]]; then
     echo "Genesis file found. Starting Opnode."
     cd /opstack/optimism/op-node
     ./bin/op-node \
-        --l2=http://localhost:8551 \
+        --l2=http://opgeth:8551 \
         --l2.jwt-secret=./jwt.txt \
         --sequencer.enabled \
         --sequencer.l1-confs=3 \
